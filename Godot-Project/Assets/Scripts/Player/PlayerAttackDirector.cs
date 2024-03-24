90b336f8ff6a79255cf4dc28bc629fcf60730000
kjtkanaly <kjtkanaly@gmail.com>
2024-03-24 19:40:48 -0400

    Moved the AttackObject class fromt he Player Attack Director class

--- Godot-Project/Assets/Scripts/Player/PlayerAttackDirector.cs
+++ Godot-Project/Assets/Scripts/Player/PlayerAttackDirector.cs
@@ -2,34 +2,6 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-public class AttackObject 
-{
-	public PlayerAttackInfo attackInfo = null;
-	public Timer timer = null;
-
-	public AttackObject(PlayerAttackInfo attackInfoObj, Timer timerObj) {
-		attackInfo = attackInfoObj;
-		timer = timerObj;
-
-		timer.Timeout += CallAttack;
-
-		UpdateTimerTime();
-		StartTimer();
-	}
-
-	private void UpdateTimerTime() {
-		timer.WaitTime = attackInfo.delay;
-	}
-
-	private void StartTimer() {
-		timer.Start();
-	}
-
-	private void CallAttack() {
-		GD.Print($"Attack Index {attackInfo.attackIndex}: Time Delay = {attackInfo.delay}s");
-	}
-}
-
 public partial class PlayerAttackDirector : Node3D
 {
 	//-------------------------------------------------------------------------
