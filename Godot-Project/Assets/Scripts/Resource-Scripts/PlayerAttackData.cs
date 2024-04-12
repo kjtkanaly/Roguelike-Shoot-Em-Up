24c06dcaa894b89aaef1da1d6fa978d031fea19d
kjtkanaly <kjtkanaly@gmail.com>
2024-04-11 22:10:53 -0400

    Working on Area of Effect attacks

--- Godot-Project/Assets/Scripts/Resource-Scripts/PlayerAttackData.cs
+++ Godot-Project/Assets/Scripts/Resource-Scripts/PlayerAttackData.cs
@@ -4,11 +4,15 @@ using System;
 [GlobalClass]
 public partial class PlayerAttackData : Resource
 {
+    // General Parameters
     [Export] public string id = "Default-001";
     [Export] public AttackObject.Type type = AttackObject.Type.None;
 	[Export] public float damage = 1.0f;
 	[Export] public float delay = 1.0f;
     [Export] public int maxLevel = 6;
+    // Projectile Parameters
     [Export] public PackedScene projectile;
     [Export] public float projectileSpeed = 1.0f;
+    // Area of Effect Parameters
+    [Export] public Mesh areaMesh;
 }
