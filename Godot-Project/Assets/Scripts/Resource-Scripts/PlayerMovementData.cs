013163248ad12b6e448ceb5c423bd6c6c56af649
kjtkanaly <kjtkanaly@gmail.com>
2024-04-17 22:34:07 -0400

    Created MovementData resource class to serve as parent for player and npc movement data classes

--- Godot-Project/Assets/Scripts/Resource-Scripts/PlayerMovementData.cs
+++ Godot-Project/Assets/Scripts/Resource-Scripts/PlayerMovementData.cs
@@ -2,17 +2,11 @@ using Godot;
 using System;
 
 [GlobalClass]
-public partial class PlayerMovementData : Resource
+public partial class PlayerMovementData : MovementData
 {
-	[Export] public float speed = 0.0f;
-	[Export] public float acceleration = 0.0f;
-	[Export] public float friction = 0.0f;
  	[Export] public float jumpVelocity = 0.0f;  
 	[Export] public float rotationSpeed = 0.0f;
 	[Export] public float rotationAcceleration = 0.0f;
 	[Export] public float mouseSensitivity = 0.0f;
 	[Export] public float rollSpeed = 0.0f;
-	[Export] public float mass = 0.0f;
-    public float gravity = ProjectSettings.GetSetting(
-                               "physics/3d/default_gravity").AsSingle();
 }
