3307fe24348c014c209033e09c1800b400d898be
kjtkanaly <kjtkanaly@gmail.com>
2024-04-21 15:03:07 -0400

    Added the npc movement director class to the npc movement dir object. Created a default npc movement data resource. Also added all the child objects for the NPC interaction director

--- Godot-Project/Assets/Scripts/General/MovementDirector.cs
+++ Godot-Project/Assets/Scripts/General/MovementDirector.cs
@@ -3,42 +3,46 @@ using System;
 
 public partial class MovementDirector : CharacterBody3D
 {
-    //-------------------------------------------------------------------------
-    // Game Componenets
-    // Private
-    private float gravity = ProjectSettings.GetSetting(
+	//-------------------------------------------------------------------------
+	// Game Componenets
+	// Private
+	private float gravity = ProjectSettings.GetSetting(
 						   "physics/3d/default_gravity").AsSingle();
-    // Public
-    // Protected 
-    protected Vector2 lateralVelocitySnapshot;
-    protected float verticalVelocitySnapshot;
-    [Export] protected MovementData movementData;
+	// Public
+	// Protected 
+	protected Vector2 lateralVelocitySnapshot;
+	protected float verticalVelocitySnapshot;
+	[Export] protected MovementData movementData;
 
-    //-------------------------------------------------------------------------
-    // Game Events
-    public override void _PhysicsProcess(double delta)
-    {
-        // Update Velocity Snapshot Variables
+	//-------------------------------------------------------------------------
+	// Game Events
+	public override void _PhysicsProcess(double delta)
+	{
+		// Update Velocity Snapshot Variables
 		lateralVelocitySnapshot = new Vector2(Velocity.X, 
 											  Velocity.Z);
 		verticalVelocitySnapshot = Velocity.Y;
 
-        ApplyGravity((float)delta);
+		ApplyGravity((float)delta);
 
-        MoveAndSlide();
-    }
+		Velocity = new Vector3(lateralVelocitySnapshot.X, 
+							   verticalVelocitySnapshot, 
+							   lateralVelocitySnapshot.Y);
 
-    //-------------------------------------------------------------------------
-    // Methods
-    protected PlayerMovementData GetPlayerData() {
-        return (PlayerMovementData) movementData;
-    }
-
-    protected void ApplyGravity(float timeDelta) {
-		if (!IsOnFloor())
-		    verticalVelocitySnapshot -= movementData.mass * gravity * timeDelta;
+		MoveAndSlide();
 	}
 
-    //-------------------------------------------------------------------------
+	//-------------------------------------------------------------------------
+	// Methods
+	protected PlayerMovementData GetPlayerData() {
+		return (PlayerMovementData) movementData;
+	}
+
+	protected void ApplyGravity(float timeDelta) {
+		if (!IsOnFloor())
+			verticalVelocitySnapshot -= movementData.mass * gravity * timeDelta;
+	}
+
+	//-------------------------------------------------------------------------
 	// Debug Methods
 }
