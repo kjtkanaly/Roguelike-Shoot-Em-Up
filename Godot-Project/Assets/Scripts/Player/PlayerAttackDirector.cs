47ee26ef2c3d7071593a42643b2e8d0e9d38780a
kjtkanaly <kjtkanaly@gmail.com>
2024-05-10 14:23:08 -0400

    Nevermind, i forogot that the AoE and projectile attacks utilze node 3D shit. So the parent class 'playerattackdirector' has been changed back to node3D.

--- Godot-Project/Assets/Scripts/Player/PlayerAttackDirector.cs
+++ Godot-Project/Assets/Scripts/Player/PlayerAttackDirector.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class PlayerAttackDirector : Node
+public partial class PlayerAttackDirector : Node3D
 {
 	//-------------------------------------------------------------------------
 	// Game Componenets
