c060a94d4b50893403f61b644ecd7e467b2b11f0
kjtkanaly <kjtkanaly@gmail.com>
2024-06-01 17:00:32 -0400

    Moved all the interaction classes into one dir path. Also created the interatction class structure diagram.

---
+++ Godot-Project/Assets/Scripts/Interaction/InteractionDirector.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class InteractionDirector : Node3D
+{
+    //-------------------------------------------------------------------------
+    // Game Componenets
+    // Public
+
+    // Protected
+
+    // Private
+
+    //-------------------------------------------------------------------------
+	// Game Events
+
+    //-------------------------------------------------------------------------
+	// Methods
+    // Public
+
+    // Protected
+
+    // Private
+
+    //-------------------------------------------------------------------------
+	// Debug Methods
+}
+
+// If you want to denote an area for future devlopement mark with it
+// with a to do comment. Example,
+// TO DO: Do some shit
