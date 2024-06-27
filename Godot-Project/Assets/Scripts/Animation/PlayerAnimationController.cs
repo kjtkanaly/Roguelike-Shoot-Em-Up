fc3aaa3d62b44b15e12c414d0733adb906768d96
kjtkanaly <kjtkanaly@gmail.com>
2024-06-26 20:43:16 -0400

    Working on importing player model and controlling animations. Updated the gitignore

---
+++ Godot-Project/Assets/Scripts/Animation/PlayerAnimationController.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class PlayerAnimationController : AnimationController
+{
+    //-------------------------------------------------------------------------
+    // Game Componenets
+    // Public
+
+    // Protected
+
+    // Private
+
+    //-------------------------------------------------------------------------
+	// Game Events
+
+    //-------------------------------------------------------------------------
+	// Methods
+    // Public
+
+    // Protected
+
+    // Private
+
+    //-------------------------------------------------------------------------
+	// Debug Methods
+}
