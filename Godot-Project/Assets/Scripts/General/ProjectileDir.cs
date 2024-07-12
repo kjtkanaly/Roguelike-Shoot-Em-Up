1a3761eef468cdcb3f9ca397ea7bdacc913121b3
kjtkanaly <kjtkanaly@gmail.com>
2024-07-12 13:24:46 -0400

    merge fixes

--- Godot-Project/Assets/Scripts/General/ProjectileDir.cs
+++ Godot-Project/Assets/Scripts/General/ProjectileDir.cs
@@ -3,33 +3,33 @@ using System;
 
 public partial class ProjectileDir : RigidBody3D
 {
-    //-------------------------------------------------------------------------
-    // Game Componenets
-    // Private
-    private Timer despawnTimer = null;
-    private bool debug = false;
-    // Public
-    public float damage = 0.0f;
+	//-------------------------------------------------------------------------
+	// Game Componenets
+	// Private
+	private Timer despawnTimer = null;
+	private bool debug = false;
+	// Public
+	public float damage = 0.0f;
 
-    //-------------------------------------------------------------------------
-    // Game Events
-    public override void _Ready()
-    {
-        despawnTimer = GetNode<Timer>("Despawn-Timer");
-        despawnTimer.Start();
-        despawnTimer.Timeout += Despawn;
-    }
+	//-------------------------------------------------------------------------
+	// Game Events
+	public override void _Ready()
+	{
+		despawnTimer = GetNode<Timer>("Despawn-Timer");
+		despawnTimer.Start();
+		despawnTimer.Timeout += Despawn;
+	}
 
-    //-------------------------------------------------------------------------
-    // Methods
-    private void Despawn() {
-        if (debug) {
-            GD.Print("Despawn projectile");
-        }
+	//-------------------------------------------------------------------------
+	// Methods
+	private void Despawn() {
+		if (debug) {
+			GD.Print("Despawn projectile");
+		}
 
-        QueueFree();
-    }
+		QueueFree();
+	}
 
-    //-------------------------------------------------------------------------
-    // Debug Methods
+	//-------------------------------------------------------------------------
+	// Debug Methods
 }
