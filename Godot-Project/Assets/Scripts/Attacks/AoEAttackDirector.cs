9cf51240b975c01733c988491d83bb88cd52d804
kjtkanaly <kjtkanaly@gmail.com>
2024-07-12 15:46:44 -0400

    About to get the AoE working again. Need to resolve a bug first with a null ref at line 121

--- Godot-Project/Assets/Scripts/Attacks/AoEAttackDirector.cs
+++ Godot-Project/Assets/Scripts/Attacks/AoEAttackDirector.cs
@@ -4,110 +4,130 @@ using System.Collections.Generic;
 
 public partial class AoEAttackDirector : RepetativeAttackDirector
 {
-    //-------------------------------------------------------------------------
-    // Game Componenets
-    // Public
-    
-    // Protected
+	//-------------------------------------------------------------------------
+	// Game Componenets
+	// Public
+	
+	// Protected
 
-    // Private
-    private AoEData data = null;
-    private int enemyCount = 0;
-    Dictionary<int, SceneTreeTimer> enemyTimers = 
-        new Dictionary<int, SceneTreeTimer>(); 
-    // GetTree().CreateTimer(1.0f)
+	// Private
+	private AoEData data = null;
+	Dictionary<ulong, SceneTreeTimer> enemyTimers = 
+		new Dictionary<ulong, SceneTreeTimer>(); 
+	// GetTree().CreateTimer(1.0f)
 
-    //-------------------------------------------------------------------------
-    // Game Events
-    public override void _Ready()
-    {
-        base._Ready();
+	//-------------------------------------------------------------------------
+	// Game Events
+	public override void _Ready()
+	{
+		base._Ready();
 
-        SetAoEObjects();
-        SetCollisionMaskValues();    
+		SetAoEObjects();
+		SetCollisionMaskValues();    
 
-        hitBoxDirector.AreaEntered += BeginAoEDamageSequence;
-        hitBoxDirector.AreaExited += EndAoEDamageSequence;
-    }
+		hitBoxDirector.AreaEntered += AddEnemyTimer;
+	}
 
-    //-------------------------------------------------------------------------
-    // Methods
-    // Public
-    public override void LoadAttackDataFile() {
-        // Start here 7/10/2024
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+
+		// Go through the dictionary and see which targets can take damage
+		CheckEnemyTimers();
+	}
+
+	//-------------------------------------------------------------------------
+	// Methods
+	// Public
+	public override void LoadAttackDataFile() {
+		// Start here 7/10/2024
 		data = (AoEData) GD.Load(dataPath);
 	}
 
-    public override AoEData GetAttackData() {
+	public override AoEData GetAttackData() {
 		return data;
 	}
 
-    public override void SetVisuals() {
-        attackMesh.Mesh = data.areaMesh;
+	public override void SetVisuals() {
+		attackMesh.Mesh = data.areaMesh;
 	}
 
 	public override void SetColliderInformation() {
-        hitBoxShape.Shape = data.areaColliderShape;
+		hitBoxShape.Shape = data.areaColliderShape;
 	}
 
-    public override void LevelUpAttack() {
-        base.LevelUpAttack();
+	public override void LevelUpAttack() {
+		base.LevelUpAttack();
 
-        Scale += new Vector3(1, 1, 1) 
+		Scale += new Vector3(1, 1, 1) 
 				 * ((AoEData) data).areaIncreaseStepSize;
 	}
 
-    public override string GetAttackId() {
+	public override string GetAttackId() {
 		return data.id;
 	}
 
-    public override int GetAttackMaxLevel() {
+	public override int GetAttackMaxLevel() {
 		return data.maxLevel;
 	}
 
-    // Protected
-    protected override void CallAttack() {
+	// Protected
+	protected override void CallAttack() {
 		if (debug) {
 			GD.Print($"AoE Attack:");
-            GD.Print($"Time Delay = {data.delay}s");
+			GD.Print($"Time Delay = {data.delay}s");
 		}
 
-        // Call AoE Attack Sequence
+		// Call AoE Attack Sequence
 	}
 
-    protected void BeginAoEDamageSequence(Area3D opposingArea) {
-        GD.Print($"{opposingArea.Name} Entered AoE");
+	protected void AddEnemyTimer(Area3D enemyArea) {
+		GD.Print($"{enemyArea.Name} Entered AoE");
 
-        InteractionDirector otherIteraction = opposingArea.GetNode<InteractionDirector>("..");
-        TimeDelayedDamageSequence(otherIteraction);
+		// Get the Opposing objects Interaction Director
+		InteractionDirector otherIteraction = 
+			enemyArea.GetNode<InteractionDirector>("..");
 
-        /*
-		// Get the Attack Information
-		RepetativeAttackData aoeData = (RepetativeAttackData) enemyArea.GetParent<AoEAttackDirector>().GetAttackData();
-        */
-	}
-
-    protected void EndAoEDamageSequence(Area3D enemyArea) {
-        GD.Print("Enemy Exited AoE");
-    }
-
-	protected void TimeDelayedDamageSequence(InteractionDirector otherIteraction) {
-        // Take the Initial Damage
+		// Safety Check
+		if (otherIteraction == null) {
+			GD.Print($"{enemyArea.Name} had no Interaction Area");
+			return;
+		}
+		
+		// Take the Initial Damage
 		otherIteraction.TickHealth(GetAttackData().damage);
-        /*
-		// Create the new AoE object
-		ActiveAoE aoe = new ActiveAoE(this, attackData);
 
-		// Start the AoE Damage Timer
-		aoe.delayTimer.Start();
-
-		// Setup the aoe to end once out of range
-		hitBoxDir.AreaExited += aoe.Destroy;
-        /**/
+		// Create the timer
+		SceneTreeTimer timer = GetTree().CreateTimer(GetAttackData().delay);
+		
+		// Log the Opposing Area and it's timer
+		enemyTimers.Add(otherIteraction.GetInstanceId(), timer);
 	}
 
-    // Private
-    private void SetAoEObjects() {
+	protected void EndAoEDamageSequence(Area3D enemyArea) {
+		GD.Print("Enemy Exited AoE");
+	}
+
+	protected void CheckEnemyTimers() {
+		foreach(KeyValuePair<ulong, SceneTreeTimer> enemyTimer in enemyTimers) {
+			if (enemyTimer.Value.TimeLeft <= 0) {
+				InteractionDirector enemyInteraction = 
+					(InteractionDirector) InstanceFromId(enemyTimer.Key);
+
+				if (enemyInteraction == null) {
+					enemyTimers.Remove(enemyTimer.Key);
+				}
+				
+				enemyInteraction.TickHealth(GetAttackData().damage);
+				
+				// Reset the EnemyTimer
+				enemyTimers[enemyTimer.Key] = GetTree().CreateTimer(GetAttackData().delay);
+			}
+		}
+	}
+
+	// Private
+	private void SetAoEObjects() {
 		hitBoxDirector = GetNode<Area3D>("Hit-Box-Director");
 		hitBoxShape = hitBoxDirector.GetNode<CollisionShape3D>("Hit-Box-Shape");
 	}
