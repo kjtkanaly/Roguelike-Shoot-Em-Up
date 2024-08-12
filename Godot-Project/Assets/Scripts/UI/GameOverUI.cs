2292cbd8350d47d764c47128935a6a9101003964
kjtkanaly <kjtkanaly@gmail.com>
2024-08-11 20:10:01 -0400

    Game over functionality setup. Need to do clean up on the feature. Also, need to populate the values

--- Godot-Project/Assets/Scripts/UI/GameOverUI.cs
+++ Godot-Project/Assets/Scripts/UI/GameOverUI.cs
@@ -6,21 +6,44 @@ public partial class GameOverUI : UI
     //-------------------------------------------------------------------------
     // Game Componenets
     // Public
+    [Export] public string titleMenuScenePath;
+    [Export] public string newGameButtonNodePath;
+    [Export] public string quitButtonNodePath;
 
     // Protected
 
     // Private
+    private Button newGameButton;
+    private Button quitButton;
 
     //-------------------------------------------------------------------------
-	// Game Events
+    // Game Events
+    public override void _Ready()
+    {
+        base._Ready();
+
+        newGameButton = GetNode<Button>(newGameButtonNodePath);
+        quitButton = GetNode<Button>(quitButtonNodePath);
+
+        newGameButton.ButtonUp += BeginNewGame;
+        quitButton.ButtonUp += LoadTitleMenu;
+    }
 
     //-------------------------------------------------------------------------
-	// Methods
+    // Methods
     // Public
 
     // Protected
 
     // Private
+
+    private void LoadTitleMenu() {
+        GetTree().ChangeSceneToFile(titleMenuScenePath);
+    }
+
+    private void BeginNewGame() {
+        GetTree().ReloadCurrentScene();
+    }
 
     //-------------------------------------------------------------------------
 	// Debug Methods
